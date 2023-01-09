51ebf81ebe141af2a97921ab16b55c3346d7a3f7
Ademola-Pamilerin <64588069+Ademola-Pamilerin@users.noreply.github.com>
2023-01-09 18:49:20 +0100

    Optimizing app

--- next.config.js
+++ next.config.js
@@ -6,4 +6,8 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+
+const withBundleAnalyser = require("@next/bundle-analyser")({
+  enabled: process.env.ANALYZE === true
+})
+module.exports = withBundleAnalyser(nextConfig)
