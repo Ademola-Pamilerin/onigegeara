7feb0358b5e9ed046a49981fd367490b9263afbd
Ademola-Pamilerin <64588069+Ademola-Pamilerin@users.noreply.github.com>
2023-02-05 21:56:27 +0100

    Fixing error

--- pages/unsubscribe.js
+++ pages/unsubscribe.js
@@ -24,6 +24,7 @@ const Unsubscribe = () => {
             setShowError(true)
             setError("please enter your email")
         }
+        
         else {
             dispatch(unsubscribe({ email }))
         }
